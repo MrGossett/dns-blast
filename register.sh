5058d7e5f210435b4aeae48a2ab5df8ddc2034f3
Tim Gossett <tim@mrgossett.com>
2022-05-10 14:59:34 -0400

    wip

--- register.sh
+++ register.sh
@@ -5,19 +5,22 @@ TLDS=`cat tlds`
 
 for tld in $TLDS; do
   domain="${ROOT_DOMAIN}${tld}"
+  grep $domain existing && continue
   printf "%s:\t" "$domain"
-  if [[ `grep $domain existing` ]]; then
-    printf "already registered\n"
-    continue
-  fi
-  sleep 5
-  available=$(aws route53domains check-domain-availability --domain-name "$domain" --query 'Availability' --output text)
+  sleep 1
+  available=$(aws route53domains check-domain-availability --domain-name "$domain" --query 'Availability' --output text --region us-east-1)
   printf "$available"
+
   if [[ "$available" == "AVAILABLE" ]]; then
     sleep 10
     aws route53domains register-domain --cli-input-json "$(jq --arg domain $domain '.DomainName = "\($domain)"' <input.json)" >/dev/null && echo $domain >> existing && printf "\tregistered"
+
   elif [[ "$available" == "UNAVAILABLE" ]]; then
     echo $domain >> existing
+
+  elif [[ "$available" != "PENDING" ]]; then
+    printf "%s:\t%s\n" "$domain" "$available" >> existing
   fi
+
   printf "\n"
 done
